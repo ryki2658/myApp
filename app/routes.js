4448f7794c0bc7c9fd6e90b81ebca8c4a101ef95
Ryan King <rking2658@gmail.com>
2017-07-10 21:02:06 -0600

    add work order app with passprt for users

---
+++ app/routes.js
@@ -0,0 +1,165 @@
+// Should probably be moved
+// Needed for Jobs table in mongoDB
+var mongojs = require('mongojs');
+var db = mongojs('workOrderApp', ['Jobs']);
+
+module.exports = function(app, passport) {
+
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    console.log('signup');
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // Add new job
+    app.get('/createJob', isLoggedIn, function(req, res){
+        db.Jobs.find(function (err, docs) {
+        // docs is an array of all the documents in mycollection
+            console.log(docs);
+            res.render('newJob', {
+                title: 'Jobs',
+                jobs: docs,
+                user: req.user._id
+            });
+        });
+        
+    });
+
+    // Table of jobs in workOrderApp collection
+    app.get('/tables', isLoggedIn, function(req, res){
+        db.Jobs.find({ user : req.user._id }).toArray(function(err, docs){ //Find all the jobs from the logged in user
+            res.render('tables', {
+                title: 'Tables',
+                jobs: docs
+            });
+        });
+    });
+    // Filter based on selection
+    app.post('/jobs/filter', function(req, res){
+        if(req.body.sort_selection == ''){  //Reload the table with all the users jobs on empty filter sort_selection
+            db.Jobs.find({ 'user' : req.user._id }).toArray(function(err,docs){
+                res.render('tables', {
+                    title : 'Tables',
+                    jobs: docs
+                });
+            });
+        } else{ //Get users filter selection from page body
+            db.Jobs.find({ 'user' : req.user._id, 'job_location' : req.body.sort_selection }).toArray(function(err, docs){
+                res.render('tables', {
+                    title: 'Tables',
+                    jobs: docs
+                });
+            });
+        }
+        
+    });
+
+    // Add new users
+    app.post('/jobs/add', isLoggedIn, function(req, res){
+
+        // Make sure fields are not empty
+        req.checkBody('job_id', 'Job Id is required ').notEmpty();
+        req.checkBody('job_location', 'Job Location is required ').notEmpty();
+        req.checkBody('job_description', 'Job Description is required ').notEmpty();
+        req.checkBody('job_notes', 'Job Notes is required ').notEmpty();
+
+        // Check for errors
+        var errors = req.validationErrors(); //errors made avaible in server.js 'gloabal variables'
+        var user = req.user._id;
+        var date = new Date();
+        db.Jobs.find(function (err, docs) {
+            if(errors){
+                console.log('ERRORS');
+                res.render('index', {
+                    title: 'Jobs',
+                    jobs: docs,
+                    user: user,
+                    errors: errors
+            });
+            // If no errors create new job
+            } else {
+                var newJob = {
+                    job_id: req.body.job_id,
+                    job_location: req.body.job_location,
+                    job_description: req.body.job_description,
+                    job_notes: req.body.job_notes,
+                    job_date: date, //Get the date on the fly
+                    user: user //Add user info to mongoDB for showing only data that this user has added in the table.ejs page
+                };
+                //Add job to MongoDB
+                db.Jobs.insert(newJob, function(err, result){
+                    if(err){
+                        console.log(err);
+                    }
+                    res.redirect('/profile');
+                });
+            }
+            console.log(newJob);
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,79 @@
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongojs = require('mongojs');
+var dbUsers = mongojs('workOrderApp', ['Users']);
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path = require('path');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+var expressValidator = require('express-validator');
+var configDB = require('./config/database.js');
+var configJobsDB = require('./config/jobs_db.js');
+// configuration ===============================================================
+//mongoose.connect(dbUsers); // connect to our database
+//mongoose.connect('mongodb://localhost/dbUseres');
+require('./config/passport')(passport); // pass passport for configuration
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// Set Static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Global vars
+app.use(function(req,res,next){
+	res.locals.errors = null;
+	next();
+});
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// required for passport
+//app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+// Changed to remove deprecated waring for not having 'resave: true, saveUninitialized: true' 
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch',
+    //name: cookie_name,
+    //store: sessionStore, // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
